40f84386c746ae5a14d19b56f7e06f9e9e1747f0
Juan <juanpanizzi@hotmail.com>
2025-02-27 23:28:05 -0300

    configurations

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from 'primereact/button';         
+import TemplateDemo from './components/Menubar';
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TemplateDemo />
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Menubar.tsx
@@ -0,0 +1,60 @@
+
+import React from 'react'; 
+import { Menubar } from 'primereact/menubar';
+import { MenuItem } from 'primereact/menuitem';
+
+export default function BasicDemo() {
+    const items: MenuItem[] = [
+        {
+            label: 'Inicio',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Contacto',
+            icon: 'pi pi-users'
+        },
+        // {
+        //     label: 'Projects',
+        //     icon: 'pi pi-search',
+        //     items: [
+        //         {
+        //             label: 'Components',
+        //             icon: 'pi pi-bolt'
+        //         },
+        //         {
+        //             label: 'Blocks',
+        //             icon: 'pi pi-server'
+        //         },
+        //         {
+        //             label: 'UI Kit',
+        //             icon: 'pi pi-pencil'
+        //         },
+        //         {
+        //             label: 'Templates',
+        //             icon: 'pi pi-palette',
+        //             items: [
+        //                 {
+        //                     label: 'Apollo',
+        //                     icon: 'pi pi-palette'
+        //                 },
+        //                 {
+        //                     label: 'Ultima',
+        //                     icon: 'pi pi-palette'
+        //                 }
+        //             ]
+        //         }
+        //     ]
+        // },
+        // {
+        //     label: 'Contact',
+        //     icon: 'pi pi-envelope'
+        // }
+    ];
+    
+    return (
+        <div className="">
+            <Menubar model={items} />
+        </div>
+    )
+}
+        
